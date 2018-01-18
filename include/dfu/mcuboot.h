580f5eed11532abf6b30447b297e990b987c7b14
Christopher Collins <ccollins@apache.org>
2018-01-17 17:40:36 -0800

    dfu: Allow inspection of boot loader state.
    Signed-off-by: Christopher Collins <ccollins@apache.org>

--- include/dfu/mcuboot.h
+++ include/dfu/mcuboot.h
@@ -8,6 +8,21 @@
 #ifndef __MCUBOOT_H__
 #define __MCUBOOT_H__
 
+/** Attempt to boot the contents of slot 0. */
+#define BOOT_SWAP_TYPE_NONE     1
+
+/** Swap to slot 1.  Absent a confirm command, revert back on next boot. */
+#define BOOT_SWAP_TYPE_TEST     2
+
+/** Swap to slot 1, and permanently switch to booting its contents. */
+#define BOOT_SWAP_TYPE_PERM     3
+
+/** Swap back to alternate slot.  A confirm changes this state to NONE. */
+#define BOOT_SWAP_TYPE_REVERT   4
+
+/** Swap failed because image to be run is not valid */
+#define BOOT_SWAP_TYPE_FAIL     5
+
 /**
  * @brief Marks the image in slot 0 as confirmed. The system will continue
  * booting into the image in slot 0 until told to boot from a different slot.
@@ -17,6 +32,14 @@
  * @return 0 on success, negative errno code on fail.
  */
 int boot_write_img_confirmed(void);
+
+/**
+ * @brief Determines the action, if any, that mcuboot will take on the next
+ * reboot.
+ * @return a BOOT_SWAP_TYPE_[...] constant on success, negative errno code on
+ * fail.
+ */
+int boot_swap_type(void);
 
 /**
  * @brief Marks the image in slot 1 as pending. On the next reboot, the system
